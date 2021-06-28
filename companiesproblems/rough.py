955254041f7deabb6688a7b293fc52f24082773c
zaid jubapu <zaidjubapu93@gmail.com>
2021-06-28 23:00:07 +0530

    rough file modified

---
+++ companiesproblems/rough.py
@@ -0,0 +1,78 @@
+'''        
+arr=[1,2,34,61]
+a=['a',"1"]
+N=4
+print(str(arr))
+
+'b=isPossible(N,arr)
+print(b)
+
+b="".join(a)
+print(b)
+c=b.split()
+print(c)
+
+a=(str(x) for x in arr)
+print(list(a))
+
+e="".join((str(x) for x in arr))
+print(e)
+e="".join([str(x) for x in arr])
+print(e)''' # finish 1
+
+#arr= [1,2,3,4]
+#print(*arr)
+
+'''a=3
+def abcd():
+    global b
+    b=5
+    print(b)
+    print(a)
+
+abcd()
+b=b+3
+print(b)'''
+
+# Python3 program to
+# print all elements that
+# appear more than once.
+ 
+# function to find
+# repeating elements
+ 
+ 
+def printRepeating(arr, n):
+ 
+    # First check all the
+        # values that are
+    # present in an array
+        # then go to that
+    # values as indexes
+        # and increment by
+    # the size of array
+    print(arr)
+    for i in range(0, n):
+        index = arr[i] % n
+        arr[index] =index+ n
+ 
+    # Now check which value
+        # exists more
+    # than once by dividing
+        # with the size
+    # of array
+    print(arr)
+    for i in range(0, n):
+        if (arr[i]/n) >= 2:
+            print(arr[i]/n, end=" ")
+ 
+ 
+# Driver code
+arr = [6, 3,9, 1, 3, 6, 6,9]
+arr_size = len(arr)
+ 
+print("The repeating elements are:")
+ 
+# Function call
+printRepeating(arr, arr_size)
+ 
