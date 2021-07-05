e9e01c00484bc5b92a52b0fba4097e2998e0847b
zaid jubapu <zaidjubapu93@gmail.com>
2021-07-05 22:55:01 +0530

    resolved the bug

--- companiesproblems/rough.py
+++ companiesproblems/rough.py
@@ -71,14 +71,14 @@ print("The repeating elements are:")
 
 printRepeating(arr, arr_size)'''
 
-def func(a,b):
+'''def func(a,b):
     if (a>1):
         func(a-2,b+2)
 
     print(b)
 
 func(4,5)
-
+'''
 
 
 
