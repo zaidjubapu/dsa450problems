47e7229002e0883ec3c634e98a8bb6fe97a46e0a
zaid jubapu <zaidjubapu93@gmail.com>
2021-09-15 14:01:07 +0530

    added the rough files

--- 450problems/01ReverseArr.py
+++ 450problems/01ReverseArr.py
@@ -1,6 +1,6 @@
 
 
-def reverseWord(s):
+'''def reverseWord(s):
     a=[]
     a[:0] = s # creating list of string characters
     for i in range(int(len(s)/2)):
@@ -16,5 +16,32 @@ if __name__ == "__main__":
     while(t>0):
         s = input()
         print(reverseWord(s))
-        t = t-1
+        t = t-1'''
+'''def reversWord(s):
+    a=[]
+    a[:0] = s
+    for i in range(int(len(a)/2)):
+        a[i],a[len(s)-1-i]=a[len(s)-1-i],a[i]
+
+    print(a)
+    b="".join(a)
+    a.pop()
+    v=a.reverse()
+    print(v)
+    return b
+
+
+
+a=input("enter the workd")
+print(reversWord(a))
+d=reversWord(a)
+#s=d.split("")
+#print(s)'''
+
+
+a=[1,2,38,2,1,]
+a.sort()
+print(a)
+z=a.reverse()
+print(a)
 
---
+++ 450problems/roughproblems/01revdigit.py
@@ -0,0 +1,49 @@
+# Python program to reverse a number
+ 
+# n = 4562
+# rev = 0
+ 
+# while(n > 0):
+#     a = n % 10
+#     rev = rev * 10 + a
+#     n = n // 10
+ 
+# print(rev)
+
+n=4560
+rev=0
+i=0
+l=[]
+while(n>0):
+    a=n%10
+    l.append(a)
+    rev=rev*10+a
+
+    n=n//10
+
+print(rev)
+print("".join(str(l)))
+
+print(str(l))
+
+# rev_num = 0
+# base_pos = 1
+ 
+# Recursive function to reverse
+# digits of num
+ 
+ 
+# def reversDigits(num):
+#     global rev_num
+#     global base_pos
+#     if(num > 0):
+#         reversDigits((int)(num / 10))
+#         rev_num += (num % 10) * base_pos
+#         base_pos *= 10
+#     return rev_num
+ 
+ 
+# # Driver Code
+# num = 456200
+# print("Reverse of no. is ",
+#       reversDigits(num))
