76daca14c4b07ac0293ea9967fe2a5cc574fab57
zaid jubapu <zaidjubapu93@gmail.com>
2022-04-01 15:38:41 +0530

    revesion 2

--- 450problems/roughproblems/revsion1.py
+++ 450problems/roughproblems/revsion1.py
@@ -1,5 +1,9 @@
-a=[1,5,3,2,6]
+# 1 reverse arr problem
+'''a=[1,5,3,2,6]
 for i in range(int(len(a)/2)):
     a[i],a[len(a)-i-1] = a[len(a)-i-1],a[i]
 
-print(a)
+print(a)'''
+
+# 2 kth element problem
+
