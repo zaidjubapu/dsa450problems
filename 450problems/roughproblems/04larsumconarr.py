1497281ed766cda88c7185fa08600a1fa667d2f7
zaid jubapu <zaidjubapu93@gmail.com>
2022-04-10 23:14:04 +0530

    edited

--- 450problems/roughproblems/04larsumconarr.py
+++ 450problems/roughproblems/04larsumconarr.py
@@ -1,4 +1,4 @@
-arr=[4,5,6,-23,5,43,-50,3,5,54]
+arr=[4,5,6,-23,5,43,-99,3,5,54]
 
 maxsofar=0
 maxendhere=0
@@ -6,4 +6,10 @@ maxendhere=0
 for i in range(len(arr)):
     maxendhere = maxsofar + arr[i]
 
-    if maxsofar < 
+    if maxendhere < 0:
+        maxendhere = 0
+
+    if maxendhere > maxsofar:
+        maxsofar = maxendhere
+
+print(maxsofar)
